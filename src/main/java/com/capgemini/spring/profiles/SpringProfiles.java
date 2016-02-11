bc6a3ecd58150a397e774b6e5f210657ea1a4bda
Andrew Harmel-Law <andrew.harmel-law@capgemini.com>
2016-02-11 09:46:56 +0000

    First commit

---
+++ src/main/java/com/capgemini/spring/profiles/SpringProfiles.java
@@ -0,0 +1,30 @@
+package com.capgemini.spring.profiles;
+
+/**
+ * A class that defines standard spring profiles
+ *
+ * @author Abbas Attarwala
+ * @author Andrew Harmel-Law
+ */
+public interface SpringProfiles {
+
+    /**
+     * The 'default' profile would be expected to use prod-like dependencies for everything
+     */
+    String DEFAULT_PROFILE_NAME = "default";
+
+    /**
+     * The 'stubbed' profile would be expected to use stubbed (i.e. local) service activators
+     */
+    String STUBBED_PROFILE_NAME = "stubbed";
+
+    /**
+     * The 'embeddedAMQ' profile would be expected to use embedded ActiveMQ but prod-like everything else
+     */
+    String EMBEDDED_AMQ_PROFILE_NAME = "embeddedAMQ";
+
+    /**
+     * The 'noDB' profile would be expected to use stubbed services to save and retrieve all data
+     */
+    String NO_DB = "noDB";
+}
